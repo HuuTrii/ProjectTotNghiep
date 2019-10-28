d9680f1802f41c55cc4de4b3a3ad9e6c4872668a
ai-nguyen <lukhachvaquantro@gmail.com>
2019-10-28 18:41:30 +0700

    c

--- WebQuanLyKho/resources/views/page/vendor/vendor1.blade.php
+++ WebQuanLyKho/resources/views/page/vendor/vendor1.blade.php
@@ -5,17 +5,17 @@
 <div class="content-wrapper">
     <section class="content-header">
         <h1>
-           Nhà Cung Cấp
+           Sản phẩm
         </h1>
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="#"><i class="fa fa-dashboard"></i> Quản lý</a></li>
-            <li class="breadcrumb-item"><a href="#">Nhà Cung Cấp</a></li>
+            <li class="breadcrumb-item"><a href="#"> Sản phẩm</a></li>
         </ol>
     </section>
     <section class="content">
         <div class="box">
             <div class="box-header with-border">
-                <h3 class="box-title">Nhà cung cấp</h3>
+                <h3 class="box-title"> Sản phẩm</h3>
                 <h6 class="box-subtitle">Danh Sách</h6>
             </div>
             <!-- /.box-header -->
@@ -33,22 +33,22 @@
     <form id="addform" method="POST">
         <div class="modal-content">
             <div class="modal-header">
-                <h3 class="modal-title">Thêm Nhà Cung Cấp</h3>
+                <h3 class="modal-title">Thêm Sản phẩm</h3>
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
             </div>
             <div class="modal-body">
                 {{csrf_field()}}
                         <div class="form-group">
-                            <label>Tên Nhà Cung Cấp</label>
-                            <input name="name_vendor"  type="text" placeholder="Tên nhà cung cấp" class="form-control">
+                            <label>Tên Sản phẩm</label>
+                            <input name="name_pro"  type="text" placeholder="Tên Sản phẩm" class="form-control">
                         </div>
                         <div class="form-group">
-                            <label>Địa Chỉ</label>
-                            <input name="adress"  type="text" placeholder="Địa Chỉ" class="form-control">
+                            <label>Hình ảnh</label>
+                            <input name="images"  type="text" placeholder="Hình ảnh" class="form-control">
                         </div>
                         <div class="form-group">
-                            <label>Email</label>
-                            <input name="email"  type="email" placeholder="Email" class="form-control">
+                            <label>price</label>
+                            <input name="price"  type="price" placeholder="price" class="form-control">
                         </div>
                         <div class="form-group">
                             <label>Điện Thoại</label>
@@ -84,16 +84,16 @@
                     {{method_field('PUT')}}
                     <input type="hidden" name="id" id="id">
                     <div class="form-group">
-                        <label>Tên Nhà Cung Cấp</label>
-                        <input type="text" class="form-control" name="name_vendor" id="name_vendor" placeholder="Tên Nhà Cung Cấp">
+                        <label>Tên Sản phẩm</label>
+                        <input type="text" class="form-control" name="name_pro" id="name_pro" placeholder="Tên Sản phẩm">
                     </div>
                     <div class="form-group">
-                        <label>Địa Chỉ</label>
-                        <input type="text" class="form-control" name="adress" id="adress" placeholder="Địa Chỉ">
+                        <label>Hình ảnh</label>
+                        <input type="text" class="form-control" name="images" id="images" placeholder="Hình ảnh">
                     </div>
                     <div class="form-group">
-                        <label>Email</label>
-                        <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+                        <label>Đơn giá</label>
+                        <input type="text" class="form-control" name="price" id="price" placeholder="Đơn giá">
                     </div>
                     <div class="form-group">
                         <label>Điện Thoại</label>
@@ -112,18 +112,18 @@
                         <table id="data_vendor" class="table table-bordered table-hover display nowrap margin-top-10 w-p100 dataTable" role="grid" aria-describedby="example_info">
                                 <tr role="row">
                                     <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 10%;">Mã</th>
-                                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 60%;">Tên Nhà Cung Cấp</th>
-                                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 60%;">Địa Chỉ</th>
-                                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 60%;">Email</th>
+                                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 60%;">Tên Sản phẩm</th>
+                                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 60%;">Hình ảnh</th>
+                                    <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 60%;">price</th>
                                     <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 60%;">Điện Thoại</th>
                                     <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 30%;">Tương tác</th>
                                 </tr>
                                  @foreach($vendor as $vendor1)
                                         <tr role="row " class="odd ">
                                             <td>{{$vendor1->id_vendor}}</td>
-                                            <td>{{$vendor1->name_vendor}}</td>
-                                            <td>{{$vendor1->adress}}</td>
-                                            <td>{{$vendor1->email}}</td>
+                                            <td>{{$vendor1->name_pro}}</td>
+                                            <td>{{$vendor1->images}}</td>
+                                            <td>{{$vendor1->price}}</td>
                                             <td>{{$vendor1->phone_number}}</td>
                                             <td>
                                             <a href=" {{ route('vendorDestroy', $vendor1->id_vendor )}}">  <span class="badge badge-danger" style="padding: 1% 5%;"><i class="fa fa-trash"></i></span></a>
@@ -158,7 +158,7 @@
             ajax: '{{asset('datatable/list')}}',
             columns: [
                 // { data: 'id_vendor', name: 'id_vendor' },
-                // { data: 'name_vendor', name: 'name_vendor' },
+                // { data: 'name_pro', name: 'name_pro' },
 
 
             ]
@@ -185,9 +185,9 @@
             }).get();
             console.log(data);
             $('#id').val(data[0]);
-            $('#name_vendor').val(data[1]);
-            $('#adress').val(data[2]);
-            $('#email').val(data[3]);
+            $('#name_pro').val(data[1]);
+            $('#images').val(data[2]);
+            $('#price').val(data[3]);
             $('#phone_number').val(data[4]);
         });
         $(editform).on('submit',function (e) {
